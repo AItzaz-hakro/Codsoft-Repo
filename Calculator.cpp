ce05df38ded8bd50ec3325e30811435fc33f5b89
Aitzaz Hassan <146619431+AItzaz-hakro@users.noreply.github.com>
2024-01-03 11:35:17 +0500

    Add files via upload

--- Calculator.cpp
+++ Calculator.cpp
@@ -3,51 +3,127 @@
 #include<conio.h>
 #include<cstdlib>
 using namespace std;
-int i=0;
-int k=i;
-void add(int x){
-    for( i=0; i<k; i++){
-        cout<<i;
-    }
-    
+int x,y;
+  
+  class GPA{
 
-};
-void sub(int x){
+    public:
 
-};
-void div(int x){
+        int n=x,m=y;
 
-};
-void mult(int x){
+        void theory(){
+          cout<<"Number of Theory Subjects: ";
+          cin>>n;
+          for(int i=1; i<=n; i++){
+            int f;
+            cout<<"Enter Theory marks in  Subject "<<i<<" :  ";
+            cin>>f;
+          }
+         }
 
-};
+
+          void practical(){
+          
+          cout<<"Number of Practical Subjects: ";
+          cin>>m;
+
+         for(int i=1; i<=m; i++){
+            int f;
+            cout<<"Enter Theory marks in  Subject "<<i<<" :  ";
+            cin>>f;
+          }
+         }
+
+         void calculation(){
+          float gpa;
+          // gpa = a1+ a2;
+
+         }
+  
+     };
 
 
+  
+   class Calculator{
+
+    public:
+
+       void menu(float  x, float y){
+
+
+           float add = x + y;
+           float sub = x - y;
+           float mult = x * y;
+           float div = x / y;
+          //  int mod = x % y;
+
+          cout<<"1) Addition"<<endl;
+          cout<<"2) Subtraction"<<endl;
+          cout<<"3) Multiplication"<<endl;
+          cout<<"4) Division"<<endl;
+          // cout<<"5) Modulus"<<endl;
+           int z;
+           cout<<endl<<"   Selction : " ;
+           cin>>z;
+
+           
+
+           if(z == 1){
+          cout<<"Addition: "<<add<<endl;}
+          else if(z == 2){
+          cout<<"Subtraction: "<<sub<<endl;
+5
+          } else if(z == 3){
+          cout<<"Multiplication: "<<mult<<endl;
+            
+          } else if(z == 4){
+          cout<<"Division: "<<div<<endl;
+        
+          }
+             else {
+              cout<<"Invalid Selection: "<<endl;
+             }
+       
+      }
+
+
+   };
+
+
+
+   
 int main(){
-     
+      int a;
+
+      cout<<endl<<"1) GPA calculation"<<endl;
+      cout<<endl<<"2) Mathematical Calculation";
+      cout<<endl<<"3) Exit"<<endl;
+      cout<<endl<<"Selection ";
+      cin>>a;
+
+      if (a==1){
     
+    GPA first;   
+    
+      cout<<endl<<"***********GPA Calculator********* \n\n"<<endl;
 
-   int j;
-   cout<<"Enter Number count: ";
-   cin >> j;
-
-    int k;
-   for(int i=1; i<=j; i++){
-    cout<<"Enter"<<i<<"Number  : ";
-    k=i;
-    cin>>k;
-    cout<<endl;
-    }
-    cout<<setw(50)<<"ENTER OPERATION "<<endl;
-    cout<<"1) ADDITON "<<endl;
-    cout<<"2) SUBRACTION "<<endl;
-    cout<<"3) DIVISION "<<endl;
-    cout<<"4) MULTIPICATION "<<endl;
-     int  f;
-     cin >> f;
-       add(f);
-
-     
+    first.print();
+    first.theory();
+    first.practical();
+      }
+     else if(a==2){
+      Calculator first;
+          int r,s;
+          cout<<"Enter Number 1: ";
+          cin>>r;
+          cout<<"Enter Number 2: ";
+          cin>>s;
+          
+       first.menu(r,s);
 
      }
+     
+     return 0;
+     }
        
+
